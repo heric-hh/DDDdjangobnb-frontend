b1e73d022bc51b3b746ada56b6c5e1a0d8089603
heric-hh <erhh.015@gmail.com>
2025-02-28 21:35:25 -0600

    feat: (ui) landlord page (static)

---
+++ app/components/ContactButton.tsx
@@ -0,0 +1,9 @@
+const ContactButton = () => {
+  return (
+    <div className="mt-6 py-4 px-6 bg-airbnb text-white rounded-xl cursor-pointer hover:bg-airbnbDark transition">
+      Contact
+    </div>
+  );
+};
+
+export default ContactButton;
